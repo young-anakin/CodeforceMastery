fbcb0189d15811d8d9d9d9159d5afc9b967ad42c
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-12-19 23:35:48 +0300

    new updates

--- B_Replace_Character.py
+++ B_Replace_Character.py
@@ -1,20 +1,11 @@
 t = int(input())
-from collections import Counter
 for _ in range(t):
-    n = int(input())
-    val = input()
-    cp = Counter(val)
-    mx = max(cp.values())
-    need = ""
-    for key, va in cp.items():
-        if va == mx:
-            need += key
-            break
-
-    arr = [val[i] for i in range(n)]
-    for i in range(n):
-        if arr[i] != need:
-            arr[i] = need
-            break
-    print("".join(arr))
-    # print(need)
+    a, b = list(map(int, input().split(" ")))
+    arr = list(map(int, input().split(" ")))
+    xx = list(sorted(arr))
+    if xx == arr:
+        print("YES")
+    elif b == 1:
+        print("NO")
+    else:
+        print("YES")
